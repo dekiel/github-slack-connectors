cb6beeaacc767f348ba7d4a93bfe4f2eab760890
dekiel <przemyslaw.pokrywka@sap.com>
2021-01-12 17:20:10 +0100

    debugging

--- slackClientFunction/handler.py
+++ slackClientFunction/handler.py
@@ -9,17 +9,25 @@ def main(event, context):
 	print(context)
 	client = WebClient(base_url=os.environ['KYMA_SLACK_KYMA_SLACK_CONNECTOR_277A5551_00A9_49DB_9B9A_FBFD891BD070_GATEWAY_URL'])
 	label = event["data"]["label"]["name"]
+	print(label)
 	title = event["data"]["issue"]["title"]
+	print(title)
 	number = event["data"]["issue"]["number"]
+	print(number)
 	repo = event["data"]["repository"]["name"]
+	print(repo)
 	try:
 		assignee = "Issue {} in repository {} is assigned to `{}`.".format(number, repo, event["data"]["issue"]["assignee"]["login"])
 	except TypeError:
 		assignee = "Issue {} in repository {} is not assigned.".format(number, repo)
+	print(assignee)
 	sender = event["data"]["sender"]["login"]
+	print(sender)
 	issue_url = event["data"]["issue"]["html_url"]
+	print(issue_url)
 	# Run only for internal-incident and customer-incident labels
 	if (label == "internal-incident") or (label == "customer-incident"):
+		print("run postMessage")
 		try:
 			response = client.chat_postMessage(channel='kyma-prow-dev-null',
 											   blocks=[
@@ -54,9 +62,10 @@ def main(event, context):
 														}
 												},
 												])
-			assert response["ok"]
+			#assert response["ok"]
+			print(response)
 		except SlackApiError as e:
 			# You will get a SlackApiError if "ok" is False
-			assert e.response["ok"] is False
-			assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+			#assert e.response["ok"] is False
+			#assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
 			print(f"Got an error: {e.response['error']}")
